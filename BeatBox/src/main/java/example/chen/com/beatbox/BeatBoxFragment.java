fbe0749b4f78f27cb8391c3fdadb993f4dac4a7a
chenyongda2018 <1243724041@qq.com>
2019-04-29 08:11:13 +0800

    init CriminalIntent

---
+++ BeatBox/src/main/java/example/chen/com/beatbox/BeatBoxFragment.java
@@ -0,0 +1,9 @@
+package example.chen.com.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
---
+++ CriminalIntent/src/main/java/example/chen/com/criminalintent/Controller/CrimeFragment.java
@@ -0,0 +1,6 @@
+package example.chen.com.criminalintent.Controller;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+}
